97887093236e018ab9b2c1d44e3e3cd6f4d10d87
Arbaj Shaikh <sarbaj637@gmail.com>
2025-09-03 22:20:02 +0530

    my commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,34 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from "react";
+import "../styles/index.css";
+import "../styles/App.css";
 
+function App() {
+  const [text, setText] = useState();
+  function updateInput(event) {
+    setText(event.target.value);
+  }
+  return (
+    <div
+      className="card-container"
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        height: "100vh",
+        flexWrap: "wrap",
+      }}
+    >
+      <div className="card" style={{ width: "50%", height: "100vh" }}>
+        <textarea value={text} onChange={updateInput} />
+      </div>
+      <div
+        className="card"
+        style={{ width: "50%", backgroundColor: "#999292ff", height: "100vh" }}
+      >
+        {text}
+      </div>
+    </div>
+  );
+}
+
+export default App;
